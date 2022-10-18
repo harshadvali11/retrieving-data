c65e5781dffdda4c485e0587f627a5ca4696c773
harshadvali11 <harshadvali1431@gmail.com>
2022-10-18 18:02:38 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 import imp
 from django.shortcuts import render
 from app.models import *
-
+from django.db.models.functions import Length
 # Create your views here.
 def display_topic(request):
     T=Topic.objects.all()
@@ -10,5 +10,25 @@ def display_topic(request):
 
 def display_webpage(request):
     W=Webpage.objects.all()
+    #W=Webpage.objects.filter(topic_name='Cricket')
+    #W=Webpage.objects.exclude(topic_name='Cricket')
+    W=Webpage.objects.all().order_by('name')
+    W=Webpage.objects.all().order_by('-name')
+    W=Webpage.objects.filter(topic_name='Volley Ball').order_by('name')
+    W=Webpage.objects.filter(topic_name='Cricket')
+    W=Webpage.objects.order_by(Length('name'))
+    W=Webpage.objects.order_by(Length('name').desc())
+    W=Webpage.objects.all()[:4:]
     d={'webpages':W}
     return render(request,'display_webpage.html',d)
+
+def display_access(request):
+    A=AccessRecords.objects.all()
+    d={'access':A}
+    return render(request,'display_access.html',d)
+
+
+
+
+
+
--- project2/urls.py
+++ project2/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('display_topic/',display_topic,name='display_topic'),
     path('display_webpage/',display_webpage,name='display_webpage'),
+    path('display_access/',display_access,name='display_access'),
 ]
