5593482dfb5325ea9370d97a1497bbccfb65a650
harshadvali11 <harshadvali1431@gmail.com>
2022-10-20 18:09:38 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -26,7 +26,7 @@ def display_webpage(request):
     W=Webpage.objects.filter(name__in=('pawan','ashu'))
     W=Webpage.objects.filter(name__regex='\w{8}')
     W=Webpage.objects.filter(Q(topic_name='Cricket') | Q(topic_name='Chess'))
-    #W=Webpage.objects.all()
+    W=Webpage.objects.all()
     
     d={'webpages':W}
     return render(request,'display_webpage.html',d)
@@ -42,6 +42,40 @@ def display_access(request):
     A=AccessRecords.objects.filter(date__lte='1999-10-26')
     A=AccessRecords.objects.filter(date__year__gt='1999')
     
+def update_webpage(request):
+    #Webpage.objects.filter(topic_name='Cricket').update(name='Virat')
+    #Webpage.objects.filter(topic_name='Volley Ball').update(name='Kabir')
+    #Webpage.objects.filter(name='basha').update(topic_name='Cricket')
+    #Webpage.objects.filter(name='basha shaik').update(topic_name='Cricket')
+    #Webpage.objects.update_or_create(topic_name='Chess',defaults={'name':'Charan Kumar','url':'https://charan.in'})
+    #Webpage.objects.update_or_create(topic_name='Cricket',defaults={'name':'MSD','url':'https://MSD.in'})
+    T=Topic.objects.get_or_create(topic_name='Cricket')[0]
+    T.save()
+    Webpage.objects.update_or_create(name='Kiran',defaults={'topic_name':T,'url':'https://kiran.in'})
+    W=Webpage.objects.all()
+    d={'webpages':W}
+    return render(request,'display_webpage.html',d)
+
+
+
+def delete_webpage(request):
+    #Webpage.objects.filter(topic_name='Volley Ball').delete()
+    Webpage.objects.all().delete()
+
+    W=Webpage.objects.all()
+    d={'webpages':W}
+    return render(request,'display_webpage.html',d)
+
+
+
+
+
+
+
+
+
+
+
     
     
     
